7a8df74905d48f11733a1affbe89ce4f54b1ce7b
Marcus Sjöblom <marsj402@student.liu.se>
2023-12-18 14:10:40 +0100

    Changed minor value

--- functions.js
+++ functions.js
@@ -12,7 +12,7 @@ export function setupSceneAndControls() {
     75,
     window.innerWidth / window.innerHeight,
     0.1,
-    100000
+    1000
   );
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
