f8d9ac11a7c1c83219c8dfedb2baaf0e7e30b85c
rasmussvala <rassv453@student.liu.se>
2023-12-28 17:17:24 +0100

    fbm in threejs

--- main.js
+++ main.js
@@ -20,13 +20,13 @@ document.body.appendChild(renderer.domElement);
 
 // Set up controls
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.autoRotate = true;
-controls.autoRotateSpeed = 1.0;
+// controls.autoRotate = true;
+// controls.autoRotateSpeed = 1.0;
 controls.enableDamping = true;
 
 // Add axis helper
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
+// const axesHelper = new THREE.AxesHelper(5);
+// scene.add(axesHelper);
 
 // Create a lightSource, so we can see where the light is coming from (WIP)
 // const lightGeometry = new THREE.SphereGeometry(0.2, 16, 16);
@@ -38,29 +38,56 @@ scene.add(axesHelper);
 // lightMesh.position.set(1, 1, 1);
 // scene.add(lightMesh);
 
+// fBm function
+function fBm(x, y, z, octaves, persistence) {
+  let total = 0;
+  let frequency = 1;
+  let amplitude = 1;
+  let maxValue = 0; // Used for normalizing result to [-1, 1]
+
+  for (let i = 0; i < octaves; i++) {
+    total +=
+      noise.simplex3(x * frequency, y * frequency, z * frequency) * amplitude;
+
+    maxValue += amplitude;
+
+    amplitude *= persistence;
+    frequency *= 2;
+  }
+
+  return total / maxValue;
+}
+
 // Create a mesh using Phong material
-const geometry = new THREE.SphereGeometry(1, 64, 64);
+const geometry = new THREE.SphereGeometry(1, 512, 512);
 
 // Get the vertices of the geometry
 const vertices = geometry.attributes.position;
 
-// Use noise to manipulate vertex positions
 for (let i = 0; i < vertices.count; i++) {
   const vertex = new THREE.Vector3(
     vertices.getX(i),
     vertices.getY(i),
     vertices.getZ(i)
   );
-  const noiseValue = noise.simplex3(
+
+  // Adjust these parameters as needed
+  const octaves = 8;
+  const persistence = 0.5;
+
+  const noiseValue = fBm(
     vertex.x * 1.5,
     vertex.y * 1.5,
-    vertex.z * 1.5
-  ); // Adjust the scale as needed
+    vertex.z * 1.5,
+    octaves,
+    persistence
+  );
 
-  // Modify the vertex position based on noise value
-  vertex.normalize().multiplyScalar(1 + 0.1 * noiseValue); // Adjust the multiplier for deformation
+  vertex.normalize().multiplyScalar(1 + 0.5 * noiseValue);
   vertices.setXYZ(i, vertex.x, vertex.y, vertex.z);
 }
+
+geometry.computeVertexNormals(); // This recalculates the normals
 
 const material = createPhongMaterial();
 const planet = new THREE.Mesh(geometry, material);
