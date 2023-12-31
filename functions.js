4afa44aa1a4bd3211c9a2d9883b307d66babdf46
rasmussvala <rassv453@student.liu.se>
2023-12-31 14:43:30 +0100

    added camera controls

--- functions.js
+++ functions.js
@@ -62,9 +62,11 @@ export function setupSceneAndControls() {
   document.body.appendChild(renderer.domElement);
 
   const controls = new OrbitControls(camera, renderer.domElement);
-  // controls.autoRotate = true;
-  // controls.autoRotateSpeed = 0.2;
+  controls.autoRotate = true;
+  controls.autoRotateSpeed = 0.2;
   controls.enableDamping = true;
+  controls.minDistance = 2;
+  controls.maxDistance = 500;
 
   // Set initial camera position and update controls
   camera.position.z = 5;
