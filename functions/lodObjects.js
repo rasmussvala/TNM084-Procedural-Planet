23009b22b69cd2d4b98ffbc1d56ccb26baf67efe
rasmussvala <rassv453@student.liu.se>
2024-02-08 15:48:06 +0100

    removed LOD

--- functions/lodObjects.js
+++ functions/lodObjects.js
@@ -1,34 +1,14 @@
 import * as THREE from "three";
 
-export function createLODObjects(terrainMaterial, waterMaterial) {
+export function createObjects(terrainMaterial, waterMaterial) {
   // Create different levels of detail
-  const highDetail = new THREE.IcosahedronGeometry(1, 128);
-  const mediumDetail = new THREE.IcosahedronGeometry(1, 32);
-  const lowDetail = new THREE.IcosahedronGeometry(1, 16);
+  const geometry = new THREE.IcosahedronGeometry(1, 128);
 
-  // Create mesh for each level of detail for the terrain
-  const highDetailMeshTerrain = new THREE.Mesh(highDetail, terrainMaterial);
-  const mediumDetailMeshTerrain = new THREE.Mesh(mediumDetail, terrainMaterial);
-  const lowDetailMeshTerrain = new THREE.Mesh(lowDetail, terrainMaterial);
+  const terrain = new THREE.Mesh(geometry, terrainMaterial);
+  const water = new THREE.Mesh(geometry, waterMaterial);
 
-  // Create mesh for each level of detail for the water
-  const highDetailMeshWater = new THREE.Mesh(highDetail, waterMaterial);
-  const mediumDetailMeshWater = new THREE.Mesh(mediumDetail, waterMaterial);
-  const lowDetailMeshWater = new THREE.Mesh(lowDetail, waterMaterial);
-
-  // Create LOD objects for the terrain and water
-  const terrain = new THREE.LOD();
-  const water = new THREE.LOD();
-
-  // Add LOD levels for the terrain
-  terrain.addLevel(highDetailMeshTerrain, 1); // closest
-  terrain.addLevel(mediumDetailMeshTerrain, 6); // medium distance
-  terrain.addLevel(lowDetailMeshTerrain, 15); // farthest
-
-  // Add LOD levels for the water
-  water.addLevel(highDetailMeshWater, 1); // closest
-  water.addLevel(mediumDetailMeshWater, 6); // medium distance
-  water.addLevel(lowDetailMeshWater, 15); // farthest
-
-  return { terrain, water };
+  return {
+    terrain,
+    water,
+  };
 }
--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import {
 } from "./functions/materials.js";
 import { setupSceneAndControls } from "./functions/setupSceneAndControls.js";
 import { createSceneObjects } from "./functions/sceneObjects.js";
-import { createLODObjects } from "./functions/lodObjects.js";
+import { createObjects } from "./functions/lodObjects.js";
 import { setupGUI } from "./functions/gui.js";
 
 // Boilerplate code - made into a function
@@ -22,8 +22,8 @@ scene.add(clouds);
 let waterMaterial = createWaterMaterial();
 let terrainMaterial = createTerrainMaterial();
 
-// Create LOD objects
-const { terrain, water } = createLODObjects(terrainMaterial, waterMaterial);
+// Create Objects
+const { terrain, water } = createObjects(terrainMaterial, waterMaterial);
 scene.add(terrain);
 scene.add(water);
 
@@ -35,9 +35,6 @@ function updateScene(deltaTime) {
   clouds.rotation.y += 0.04 * deltaTime;
 
   waterMaterial.uniforms.time.value += deltaTime;
-
-  terrain.update(camera);
-  water.update(camera);
 }
 
 function animate() {
